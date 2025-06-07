e7fa7417c7d11a0c6406b997ef0c856fef6a5fa0
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-08 00:36:27 +0530

    Add user warnings database and implement user table creation

--- darogabot.py
+++ darogabot.py
@@ -3,6 +3,15 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import sqlite3
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "user_warnings.db")
+
+def create_user_table():
+    connection = sqlite3.connect(DB_PATH)
+
+create_user_table()
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
