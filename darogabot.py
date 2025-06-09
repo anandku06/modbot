e68733eee155cf7f64685b0d7ad8322cbbc7dfca
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-09 23:09:40 +0530

    Implement profanity warning system with user banning logic

--- darogabot.py
+++ darogabot.py
@@ -8,6 +8,7 @@ import sqlite3
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "user_warnings.db")
 
+profanity = ["poopoo", "nword", "splorge"]
 
 def create_user_table():
     connection = sqlite3.connect(DB_PATH)
@@ -50,7 +51,12 @@ def increase_and_get_warnings(user_id: int, guild_id: int):
     cursor.execute("""
         UPDATE users_per_guild SET warning_count = ?
         WHERE (user_id = ?) AND (guild_id = ?);
-    """, (user_id, guild_id))
+    """, (result[0] + 1, user_id, guild_id))
+
+    connection.commit()
+    connection.close()
+
+    return result[0] + 1
 
 
 load_dotenv()
@@ -67,5 +73,23 @@ async def on_ready():
     await bot.tree.sync()
     print(f"{bot.user} is online!!")
 
+@bot.event
+async def on_message(msg):
+    if msg.author.id != bot.user.id:
+        for term in profanity:
+            if term.lower() in msg.content.lower():
+                num_warnings = increase_and_get_warnings(msg.author.id, msg.guild.id)
+
+                if num_warnings >= 3:
+                    await msg.author.ban(reason="Exceeded 3 strikes for using profanity.")
+                    await msg.channel.send(f"{msg.author.mention} has been banned for repeated profanity.")
+                else:
+                    await msg.channel.send(
+                        f"Warning {num_warnings}/3 {msg.author.mention}. If you reach 3 warnings, you'll be banned."
+                    )
+                    await msg.delete()
+                    
+                break
+    await bot.process_commands(msg)
 
 bot.run(TOKEN)
